bfc9f54c9274140b20d02399f900f67ec6516418
maitzeth <andre801mz@gmail.com>
2024-04-26 02:30:36 -0300

    feat: :sparkles: adding basic layout on App component

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,8 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <main className="bg-primary min-h-screen flex items-center justify-center">
+      <App />
+    </main>
   </React.StrictMode>,
 )
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          light: "#0b548b",
+          DEFAULT: "#084f85",
+          dark: "#003b67",
+          darker: "#00355d",
+        },
+        secondary: {
+          DEFAULT: "#17aa8d",
+        },
+      },
+    },
   },
   plugins: [],
 };
